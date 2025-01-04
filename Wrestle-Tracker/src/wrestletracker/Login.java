1713cd8899058084285f4e8ac7e71ffc2cb0cbf4
Shivam Patel <141944585+Shivamania@users.noreply.github.com>
2025-01-04 12:12:00 +0530

    Update Login.java

--- Wrestle-Tracker/src/wrestletracker/Login.java
+++ Wrestle-Tracker/src/wrestletracker/Login.java
@@ -65,27 +65,30 @@ public class Login extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == login){
-            try {
-                String username = tusername.getText();
-                String password = tpassword.getText();
-                 conn conn = new conn();
+    if (e.getSource() == login) {
+        try {
+            String username = tusername.getText();
+            String password = tpassword.getText();
+            conn conn = new conn();
 
-                 String query = "select * from login where username = '"+ username +"' and password = '"+password+"'";
-                ResultSet resultSet = conn.statement.executeQuery(query);
-                if (resultSet.next()){
-                    setVisible(false);
-                    new Main_class();
-                }else {
-                    JOptionPane.showMessageDialog(null,"Invalid Username or Password");
-                }
-            }catch (Exception E){
-                E.printStackTrace();
+            String query = "SELECT * FROM login WHERE username = ? AND password = ?";
+            PreparedStatement preparedStatement = conn.connection.prepareStatement(query);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                setVisible(false);
+                new Main_class();
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid Username or Password");
             }
-
-        } else if (e.getSource() == back) {
-            System.exit(77);
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
+    } else if (e.getSource() == back) {
+        System.exit(77);
+       }
     }
 
     public static void main(String[] args) {
